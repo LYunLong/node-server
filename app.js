273379d2c76ca3f960bac6f7f780ef9b3bc119d0
LYunLong <1056660519@qq.com>
2022-02-15 15:42:10 +0800

    db

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 const express = require('express')
 const app = express()
 
+let router = express.Router();
+let db = require('./db'); 
+
+//插入测试
+router.get('/dbtest',function(req, res){
+    let project = {id: '2', name: '666'};
+    let sqlString = 'INSERT INTO ttable SET ?';
+    let connection = db.connection();
+    db.insert(connection, sqlString, project, function(id){
+        console.log('inserted id is:' + id);
+    });
+    db.close(connection);
+    return;
+});
+
 app.use(express.static('./dist'))
 
 app.listen(9009, ()=>{
